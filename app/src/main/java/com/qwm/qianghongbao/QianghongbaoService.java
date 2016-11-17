4fea478c36cba1caeeb55ca4081b5ca74b46e4e8
qiwenming <qiwenmingshiwo@163.com>
2016-11-17 18:23:03 +0800

    修改

--- app/src/main/java/com/qwm/qianghongbao/QianghongbaoService.java
+++ app/src/main/java/com/qwm/qianghongbao/QianghongbaoService.java
@@ -9,8 +9,10 @@ import android.content.Intent;
 import android.os.Build;
 import android.os.IBinder;
 import android.os.Parcelable;
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.accessibility.AccessibilityEvent;
+import android.view.accessibility.AccessibilityNodeInfo;
 
 import java.util.List;
 
@@ -27,8 +29,9 @@ public class QianghongbaoService extends AccessibilityService {
     private static final String TAG = "QianghongbaoService";
     private final String mWechatPn = "com.tencent.mm";
     /** 红包消息的关键字*/
-    private static final String HONGBAO_TEXT_KEY = "123";
-//    private static final String HONGBAO_TEXT_KEY = "[微信红包]";
+//    private static final String HONGBAO_TEXT_KEY = "123";
+    private static final String HONGBAO_TEXT_KEY = "[微信红包]";
+    private static final String HONGBAO_GET_KEY = "领取红包";
 
     @Override
     public void onInterrupt() {
@@ -36,19 +39,27 @@ public class QianghongbaoService extends AccessibilityService {
 
     @Override
     public void onAccessibilityEvent(AccessibilityEvent event) {
-        Log.i(TAG, "onAccessibilityEvent: ---->" + event);
+        Log.i(TAG, "onAccessibilityEvent: ---->" +  event.getClassName()+"---->"+event.eventTypeToString(event.getEventType()));
         if (!mWechatPn.equals(event.getPackageName())) {//微信的报名
             return;
         }
         final int eventType = event.getEventType();
-
+        mcount = 0;
         switch (eventType) {
             case AccessibilityEvent.TYPE_NOTIFICATION_STATE_CHANGED://通知状态
                 handleNotification2(event);
                 break;
             case AccessibilityEvent.TYPE_WINDOW_STATE_CHANGED://window状态改变
-                break;
             case AccessibilityEvent.TYPE_WINDOW_CONTENT_CHANGED://window内容改变
+                String className = event.getClassName().toString();
+                if (className.equals("com.tencent.mm.ui.LauncherUI")) {//聊天界面
+                    getPacket1();
+                } else if (className.equals("com.tencent.mm.plugin.luckymoney.ui.LuckyMoneyReceiveUI")) {//红包界面
+                    openPacket1();
+                } else if (className.equals("com.tencent.mm.plugin.luckymoney.ui.LuckyMoneyDetailUI")) {//红包详情
+                    close();
+                }
+
                 break;
         }
     }
@@ -105,5 +116,146 @@ public class QianghongbaoService extends AccessibilityService {
     }
 
 
+    @TargetApi(Build.VERSION_CODES.JELLY_BEAN)
+    private void getPacket1(){//获取钱包，打开钱包界面
+        AccessibilityNodeInfo rootNode = getRootInActiveWindow();
+         recursion(rootNode,0);
+//        Log.i(TAG, "getPacket1: "+node.getText());
+//        node.performAction(AccessibilityNodeInfo.ACTION_CLICK);
+//        AccessibilityNodeInfo parentNode = node.getParent();
+//        while (parentNode!=null){
+//            Log.i(TAG, "getPacket1: while -->"+parentNode.getText());
+//            if(parentNode.isClickable()){
+//                parentNode.performAction(AccessibilityNodeInfo.ACTION_CLICK);
+//                break;
+//            }
+//            parentNode = parentNode.getParent();
+//        }
+    }
+
+   int mcount = 0;
+    /**
+     * 递归获取满足的控件
+     * @param rootNode
+     * @return
+     */
+    private void recursion(AccessibilityNodeInfo rootNode,int type) {
+//        Log.i(TAG, "recursion: ------>"+getSpace(type)+rootNode.getText()+"--->"+rootNode.getChildCount());
+        if(rootNode.getChildCount()==0){
+            if(rootNode.getText()!=null && rootNode.getText().toString().equals(HONGBAO_GET_KEY)){//获取到抢取红包的控件
+//                return rootNode;
+                rootNode.performAction(AccessibilityNodeInfo.ACTION_CLICK);
+                AccessibilityNodeInfo parentNode = rootNode.getParent();
+                while (parentNode!=null){
+                    Log.i(TAG, "getPacket1: while -->"+parentNode.getText());
+                    if(parentNode.isClickable()){
+                        mcount++;
+                        if(mcount>2) return;
+                        parentNode.performAction(AccessibilityNodeInfo.ACTION_CLICK);
+                        break;
+                    }
+                    parentNode = parentNode.getParent();
+                }
+            }
+        }else{
+            for (int i = 0; i < rootNode.getChildCount(); i++) {
+                if (rootNode.getChild(i) != null) {
+                    recursion(rootNode.getChild(i),type+1);
+                }
+            }
+        }
+//        return rootNode;
+    }
+
+    private String getSpace(int type) {
+        StringBuilder sb = new StringBuilder("|-");
+        for (int i=1;i<=type;i++){
+            sb.append("--");
+        }
+        return sb.toString();
+    }
+
+    /**
+     * 打开钱包
+     */
+    @TargetApi(Build.VERSION_CODES.JELLY_BEAN_MR2)
+    private void openPacket1(){
+        AccessibilityNodeInfo nodeInfo = getRootInActiveWindow();
+        if(nodeInfo!=null){
+            List<AccessibilityNodeInfo> nodeInfoList = nodeInfo.findAccessibilityNodeInfosByViewId("@id/b9m");
+            nodeInfo.recycle();
+            for (AccessibilityNodeInfo accessibilityNodeInfo : nodeInfoList) {
+                accessibilityNodeInfo.performAction(AccessibilityNodeInfo.ACTION_CLICK);
+            }
+        }
+    }
+
+
+
+    @TargetApi(Build.VERSION_CODES.JELLY_BEAN_MR2)
+    private void close() {
+        AccessibilityNodeInfo nodeInfo = getRootInActiveWindow();
+        if(nodeInfo!=null){
+            List<AccessibilityNodeInfo> nodeInfoList = nodeInfo.findAccessibilityNodeInfosByViewId("@id/ez");
+            nodeInfo.recycle();
+            for (AccessibilityNodeInfo accessibilityNodeInfo : nodeInfoList) {
+                Log.i(TAG, "close: "+accessibilityNodeInfo.getText());
+                accessibilityNodeInfo.performAction(AccessibilityNodeInfo.ACTION_CLICK);
+                AccessibilityNodeInfo parent = accessibilityNodeInfo.getParent();
+                while (parent != null) {
+                    if (parent.isClickable()) {
+                        parent.performAction(AccessibilityNodeInfo.ACTION_CLICK);
+                        break;
+                    }
+                    parent = parent.getParent();
+                }
+            }
+        }
+    }
+
+
+    /**
+     * 模拟点击,打开抢红包界面
+     */
+    @TargetApi(Build.VERSION_CODES.JELLY_BEAN)
+    private void getPacket() {
+        AccessibilityNodeInfo rootNode = getRootInActiveWindow();
+        AccessibilityNodeInfo node = recycle(rootNode);
+
+        node.performAction(AccessibilityNodeInfo.ACTION_CLICK);
+        AccessibilityNodeInfo parent = node.getParent();
+        while (parent != null) {
+            if (parent.isClickable()) {
+                parent.performAction(AccessibilityNodeInfo.ACTION_CLICK);
+                break;
+            }
+            parent = parent.getParent();
+        }
+    }
+
+    /**
+     * 递归查找当前聊天窗口中的红包信息
+     *
+     * 聊天窗口中的红包都存在"领取红包"一词,因此可根据该词查找红包
+     *
+     * @param node
+     */
+    public AccessibilityNodeInfo recycle(AccessibilityNodeInfo node) {
+        Log.i(TAG, "recycle: --->"+node.getText());
+        if (node.getChildCount() == 0) {
+            if (node.getText() != null) {
+                if (HONGBAO_GET_KEY.equals(node.getText().toString())) {
+                    return node;
+                }
+            }
+        } else {
+            for (int i = 0; i < node.getChildCount(); i++) {
+                if (node.getChild(i) != null) {
+                    recycle(node.getChild(i));
+                }
+            }
+        }
+        return node;
+    }
 
 }
